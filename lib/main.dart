d4200067ecfcbfb97e40f7a912cbbc9ebc2df8df
ReinerRego <regokoppany@gmail.com>
2024-02-25 22:44:29 +0100

    test

--- lib/main.dart
+++ lib/main.dart
@@ -1,65 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:refilc_teacher/screens/login/login_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Login App',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: const HomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  const HomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text("szia"),
-      ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const TextField(
-                decoration: InputDecoration(hintText: "Username"),
-                autocorrect: false,
-                autofillHints: [AutofillHints.username],
-              ),
-              const SizedBox(height: 10),
-              const TextField(
-                decoration: InputDecoration(hintText: "Password"),
-                autocorrect: false,
-                autofillHints: [AutofillHints.username],
-              ),
-              const SizedBox(height: 10),
-              FilledButton(onPressed: () {}, child: const Text("Login"))
-            ],
-          ),
-        ),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      home: LoginScreen(),
     );
   }
 }
---
+++ lib/screens/login/login_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  TextEditingController _insituteController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Column(children: [
+              TextField(controller: _insituteController, decoration: const InputDecoration(hintText: "insituteID"),),
+              FilledButton(onPressed: (){}, child: const Text("asd")),
+            ],)
+          ],
+        ),
+      ),
+    );
+  }
+}
